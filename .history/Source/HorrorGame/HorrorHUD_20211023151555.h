7381880d54e969d239083e3952c5f265b515af53
louiepow <louiepows@gmail.com>
2021-10-23 15:41:30 +1030

    Added Crosshair

---
+++ .history/Source/HorrorGame/HorrorHUD_20211023151555.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "HorrorHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORGAME_API AHorrorHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
